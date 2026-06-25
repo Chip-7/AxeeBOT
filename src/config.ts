390ac1daa161732d112ff7ca17d838949c2508e9
Adzyyy ツ <tiktuk802@gmail.com>
2026-06-26 04:03:17 +0800

    Set Configs

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+interface Configurations {
+nameBot: string,
+  type: string,
+  version: string
+}
+
+export const bot: Configurations {
+nameBot: "Axee",
+  type: "Plugins TypeScript",
+  version: "1.0.2"
+}
